f9139622d410964f579dca3d1e22607d9b3b2eec
AnnaYakovenko29 <125920571+AnnaYakovenko29@users.noreply.github.com>
2023-06-01 15:56:29 +0300

    goit-react-hw-04-feedback

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,10 +1,10 @@
 import './Notification.css'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 export const Notification = ({ message }) => {
     return <p className='titleNotification'>{message}</p>;
   };
 
   Notification.propTypes = {
-    message: PropTypes.string.isRequired,
+    message: PropTypes.string,
   };
